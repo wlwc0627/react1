a34e08771c867d7d697750cfc23712c6f392fb1d
wlwc0627 <33441607+actorWen@users.noreply.github.com>
2018-12-14 02:05:03 +0800

    test

---
+++ precache-manifest.5f47b0ba4f20c78b302d8aaaf6131747.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c58d27d3281ee858e4ff",
+    "url": "./static/js/runtime~main.c58d27d3.js"
+  },
+  {
+    "revision": "1925fd29e9648e4f9583",
+    "url": "./static/js/main.1925fd29.chunk.js"
+  },
+  {
+    "revision": "f1f97c5705b1a3ba0d3d",
+    "url": "./static/js/2.f1f97c57.chunk.js"
+  },
+  {
+    "revision": "74b8868373fb347cb3de",
+    "url": "./static/js/1.74b88683.chunk.js"
+  },
+  {
+    "revision": "e4a853ef191031598cf372c98bff5349",
+    "url": "./index.html"
+  }
+];
